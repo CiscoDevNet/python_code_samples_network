76daeeb7b5016e8363cd49db4407578bdaad2ac7
Jason Frazier <jafrazie@cisco.com>
2017-02-01 20:52:04 -0500

    Added folder for sho version Py script

---
+++ Py-sho-ver-onbox/README.md
@@ -0,0 +1,10 @@
+# NETCONF get-config
+
+This is an example Python script that literally just does "show version" on a network element.
+
+# requirements
+
+-- IOS-XE running >/= 16.5 with guestshell enabled.
+
+# running
+-- on-box.
--- Py-sho-ver-onbox/sho-ver.py
+++ Py-sho-ver-onbox/sho-ver.py
@@ -24,34 +24,10 @@
 # OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
 # SUCH DAMAGE.
 #
-# This script retrieves entire configuration from a network element via NETCONF 
-# prints it out in a "pretty" XML tree.
+# This script is designed to run on the network element itself. It runs a 
+# single exec command .. "show version"
 
-import sys
-from argparse import ArgumentParser
-from ncclient import manager
-import xml.dom.minidom
+#!/usr/bin/python
 
-if __name__ == '__main__':
-    parser = ArgumentParser(description='Select options.')
-    # Input parameters
-    parser.add_argument('--host', type=str, required=True,
-                        help="The device IP or DN")
-    parser.add_argument('-u', '--username', type=str, default='cisco',
-                        help="Go on, guess!")
-    parser.add_argument('-p', '--password', type=str, default='cisco',
-                        help="Yep, this one too! ;-)")
-    parser.add_argument('--port', type=int, default=830,
-                        help="Specify this if you want a non-default port")
-    args = parser.parse_args()
-    
-    m =  manager.connect(host=args.host,
-                         port=args.port,
-                         username=args.username,
-                         password=args.password,
-                         device_params={'name':"csr"})
-       # Pretty print the XML reply
-    xmlDom = xml.dom.minidom.parseString( str( m.get_config(source='running')))
-    print xmlDom.toprettyxml( indent = "  " )
-
-
+import cli
+print cli.execute('show version')
