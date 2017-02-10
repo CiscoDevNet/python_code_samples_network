c7b278a25cb30fce0bc5955c8018e24e2bd68581
Hank Preston <hank.preston@gmail.com>
2017-02-10 17:29:18 -0500

    Added RESTCONF Sample

---
+++ restconf_update_ipaddress/README.md
@@ -0,0 +1,76 @@
+# Calling RESTCONF with Python 
+
+This example script uses the requests Python library to interact with a RESTCONF Agent.  
+
+This Python script leverages RESTCONF to: 
+  - retrieve a list of interfaces on a device
+  - ask the user for the interface to configure 
+  - displays the interface IP information 
+  - asks user for new IP information 
+  - updates the IP address on the interface 
+  - displays the final IP information on the interface 
+  
+This script has been tested with Python 3.5, however may work with other versions.  
+    
+## DevNet Sandbox 
+
+This script targets the RESTCONF DevNet Sandbox that leverages a CSR1000v as a target.  
+
+Find details on the Sandbox [here](https://developer.cisco.com/site/devnet/sandbox/available-labs/networking/).
+
+To execute this script against a different device, update the variables that list the connectivity, management interface, and url_base for RESTCONF.  
+    
+## Requirements
+
+Python 
+
+- requests
+
+# Getting Started 
+
+* Clone the Python Examples and change into the directory.  
+
+    ```bash 
+    git clone //github.com/CiscoDevNet/python_code_samples_network
+    cd restconf_update_ipaddress
+    ```
+
+* Create and activate a virtualenv 
+
+    ```bash 
+    virtualenv venv --python=python3.5
+    source venv/bin/activate 
+    ```
+    
+* Install the requirements 
+
+    ```bash
+    pip install -r requirements.text
+    ```
+
+* Run the script
+
+    ```
+    $ python updateip.py
+
+    # Output
+    The router has the following interfaces:
+    
+      * GigabitEthernet1
+      * GigabitEthernet2
+      * GigabitEthernet3
+    
+    Which Interface do you want to configure? GigabitEthernet2
+    GigabitEthernet2
+    Starting Interface Configuration
+    Name:  GigabitEthernet2
+    IP Address:  101.101.10.10 / 255.255.255.0
+    
+    What IP address do you want to set? 9.9.9.9
+    What Subnet Mask do you want to set? 255.255.255.0
+    
+    Ending Interface Configuration
+    Name:  GigabitEthernet2
+    IP Address:  9.9.9.9 / 255.255.255.0    
+    ```
+
---
+++ restconf_update_ipaddress/updateip.py
@@ -0,0 +1,175 @@
+#!/usr/bin/env python
+"""
+    This Python script leverages RESTCONF to: 
+      - retrieve a list of interfaces on a device
+      - ask the user for the interface to configure 
+      - displays the interface IP information 
+      - asks user for new IP information 
+      - updates the IP address on the interface 
+      - displays the final IP information on the interface 
+      
+    This script has been tested with Python 3.5, however may work with other versions.  
+    
+    This script targets the RESTCONF DevNet Sandbox that leverages a CSR1000v as 
+    a target.  To execute this script against a different device, update the variables 
+    that list the connectivity, management interface, and url_base for RESTCONF.  
+    
+    Requirements: 
+      Python 
+        - requests
+              
+    
+"""
+
+import json
+import requests 
+import sys
+from collections import OrderedDict
+
+# These variables target the RESTCONF Always-On Sandbox hosted by Cisco DevNet
+HOST = 'ios-xe-mgmt.cisco.com'
+PORT = '9443'
+USER = 'root'
+PASS = 'C!sc0123'
+
+# Identifies the interface on the device used for management access 
+# Used to ensure the script isn't used to update the IP leveraged to manage device
+MANAGEMENT_INTERFACE = "GigabitEthernet1"
+
+# Create the base URL for RESTCONF calls
+url_base = "http://{h}:{p}/api".format(h=HOST, p=PORT)
+
+# Identify yang+json as the data formats 
+headers = {'Content-Type': 'application/vnd.yang.data+json',
+           'Accept': 'application/vnd.yang.data+json'}
+
+
+# Function to retrieve the list of interfaces on a device 
+def get_configured_interfaces():
+    url = url_base + "/running/interfaces?deep"
+
+    # this statement performs a GET on the specified url
+    response = requests.get(url, 
+                            auth=(USER, PASS),
+                            headers=headers, 
+                            verify=False
+                           )
+
+    # return the json as text
+    return response.json()["ietf-interfaces:interfaces"]["interface"]
+
+
+# Used to configure the IP address on an interface 
+def configure_ip_address(interface, ip): 
+    # RESTCONF URL for specific interface 
+    url = url_base + "/running/interfaces/interface/{i}".format(i=interface)
+    
+    # Create the data payload to reconfigure IP address
+    # Need to use OrderedDicts to maintain the order of elements
+    data = OrderedDict([('ietf-interfaces:interface',
+              OrderedDict([
+                            ('name', interface),
+                            ('type', 'ianaift:ethernetCsmacd'), 
+                            ('ietf-ip:ipv4',
+                                OrderedDict([
+                                  ('address', [OrderedDict([
+                                                ('ip',ip["address"]),
+                                                ('netmask',ip["mask"])
+                                              ])]
+                                  )
+                                ])
+                            ),
+                          ])
+                        )])            
+
+    # Use PUT request to update data
+    response = requests.put(url, 
+                            auth=(USER, PASS), 
+                            headers=headers, 
+                            verify=False, 
+                            json=data
+                           )
+    print(response.text)
+
+
+# Retrieve and print the current configuration of an interface 
+def print_interface_details(interface):
+    url = url_base + "/running/interfaces/interface/{i}?deep".format(i=interface)
+
+    # this statement performs a GET on the specified url
+    response = requests.get(url, 
+                            auth=(USER, PASS),
+                            headers=headers, 
+                            verify=False
+                           )
+
+    intf = response.json()["ietf-interfaces:interface"]
+    # return the json as text    
+    print("Name: ", intf["name"])
+    print("IP Address: ", intf["ietf-ip:ipv4"]["address"][0]["ip"], "/", 
+                          intf["ietf-ip:ipv4"]["address"][0]["netmask"])
+    print()
+
+    return(intf)
+
+
+# Ask the user to select an interface to configure.  Ensures input is valid and 
+# NOT the management interface
+def interface_selection(interfaces):
+    # Ask User which interface to configure 
+    sel = input("Which Interface do you want to configure? ")
+
+    # Validate interface input
+    # Must be an interface on the device AND NOT be the Management Interface
+    while sel == MANAGEMENT_INTERFACE or not sel in [intf["name"] for intf in interfaces]:
+        print("INVALID:  Select an available interface.")
+        print("          " + MANAGEMENT_INTERFACE + " is used for management.")
+        print("          Choose another Interface")
+        sel = input("Which Interface do you want to configure? ")
+    
+    return(sel)
+
+    
+# Asks the user to provide an IP address and Mask.  Data is NOT validated.  
+def get_ip_info(): 
+    # Ask User for IP and Mask
+    ip = {}
+    ip["address"] = input("What IP address do you want to set? ")
+    ip["mask"] = input("What Subnet Mask do you want to set? ")
+    return(ip)
+
+    
+def main():
+    """
+    Simple main method calling our function.
+    """        
+    # Get a List of Interfaces 
+    interfaces = get_configured_interfaces()
+
+    print("The router has the following interfaces: \n")
+    for interface in interfaces: 
+        print("  * {name:25}".format(name=interface["name"]))
+
+    print("")
+
+    # Ask User which interface to configure 
+    selected_interface = interface_selection(interfaces)
+    print(selected_interface)        
+
+    # Print Starting Interface Details
+    print("Starting Interface Configuration")
+    print_interface_details(selected_interface)
+
+    # As User for IP Address to set
+    ip = get_ip_info()
+
+    # Configure interface 
+    configure_ip_address(selected_interface, ip)
+    
+    # Print Ending Interface Details
+    print("Ending Interface Configuration")
+    print_interface_details(selected_interface)
+    
+
+if __name__ == '__main__':
+    sys.exit(main())
