6858291d16a05b10fdfc7ebb75f68c766bbb2eef
Hank Preston <hank.preston@gmail.com>
2019-03-22 09:37:31 -0400

    added try block for better error handling

--- netmiko-interface-example/netmiko-get-interface.py
+++ netmiko-interface-example/netmiko-get-interface.py
@@ -47,17 +47,20 @@ with ConnectHandler(ip = device["address"],
     # Print the raw command output to the screen
     print(interface)
 
-    # Use regular expressions to parse the output for desired data
-    name = re.search(r'interface (.*)', interface).group(1)
-    description = re.search(r'description (.*)', interface).group(1)
-    ip_info = re.search(r'ip address (.*) (.*)', interface)
-    ip = ip_info.group(1)
-    netmask = ip_info.group(2)
+    try:
+        # Use regular expressions to parse the output for desired data
+        name = re.search(r'interface (.*)', interface).group(1)
+        description = re.search(r'description (.*)', interface).group(1)
+        ip_info = re.search(r'ip address (.*) (.*)', interface)
+        ip = ip_info.group(1)
+        netmask = ip_info.group(2)
 
-    # Print the info to the screen
-    print("The interface {name} has ip address {ip}/{mask}".format(
-            name = name,
-            ip = ip,
-            mask = netmask,
+        # Print the info to the screen
+        print("The interface {name} has ip address {ip}/{mask}".format(
+                name = name,
+                ip = ip,
+                mask = netmask,
+                )
             )
-        )
+    except Exception:
+        print("There was an error, Loopback103 might not exist.")
